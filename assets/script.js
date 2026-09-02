4d387f70d2084d6fb06f979202add8323566f0a4
sevanowonline-glitch <sevanowonline@gmail.com>
2026-09-02 11:53:59 +0530

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,11 @@ document.querySelectorAll("[data-menu]").forEach(btn=>{
  btn.addEventListener("click",()=>document.querySelector("[data-nav]").classList.toggle("open"));
 });
 document.querySelectorAll("[data-nav] a").forEach(a=>a.addEventListener("click",()=>document.querySelector("[data-nav]").classList.remove("open")));
+
+
+function openSevaNowDirections() {
+  const destination = "17.776446557698108,83.22750232745385";
+  const url = "https://www.google.com/maps/dir/?api=1&destination=" +
+              encodeURIComponent(destination) + "&travelmode=driving";
+  window.open(url, "_blank");
+}
